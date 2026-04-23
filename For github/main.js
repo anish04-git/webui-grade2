053435b51adef367d46d9e67e96749cf34045d12
anish04-git <dahalanis39@gmail.com>
2026-04-23 17:06:01 +0900

    first commit

---
+++ For github/main.js
@@ -0,0 +1,5 @@
+const target = document.getElementById("target");
+target.textContent="Afternoon";
+target.style.color="red";
+target.style.fontSize ="30px";
+target.style.backgroundColor = "yellow";
