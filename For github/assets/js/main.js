10d5f98b51084a44eb75f872012b2aad1f83f54d
anish04-git <dahalanis39@gmail.com>
2026-04-30 16:14:40 +0900

    change a diffrent image form the javascript

--- For github/assets/js/main.js
+++ For github/assets/js/main.js
@@ -1,5 +1,10 @@
 let counter = 0;
 
+function increment() {
+    counter++;
+    document.getElementById('counter').textContent = counter;
+}
+
 function decrement() {
     counter--;
     document.getElementById('counter').textContent = counter;
